406d912d2ecdd6efdbffd78ad6846da5d59312d2
ranveersingh24 <p20012436@student.newinti.edu.my>
2022-10-17 22:02:22 +0800

    Files updated

--- booking/Appointment.php
+++ booking/Appointment.php
@@ -7,41 +7,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        <link href="appointmentCSS.css" rel="stylesheet" type="text/css"/>
-        <style>
-            body {
-                background-image: url('../booking/appointment3.jpg');
-                background-size: 100% 100%; 
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                width: 100%;
-                height: 100%;
-                background-color: rgba(0,147,173,0.7);
-                overflow: auto;
-            }
-            
-            a:link, a:visited {
-  background-color: #009900;
-  color: white;
-  padding: 10px 90px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-}
-
-a:hover, a:active {
-  background-color: #006600;
-}
-
-a {
-    font-size: 17px;
-    font-family: Georgia;
-}
-        </style>
+        <link href="../booking/Appointment_CSS.css" rel="stylesheet" type="text/css"/>
     </head>
 
 <body>   
+
     
+<!-- Page Wrapper-->
+
 <div class="page-wrapper">
 <!-- Main content -->
 
@@ -60,7 +33,7 @@ a {
                         <div class="form-head">
                             <h2>Book Appointment</h2>
                         </div>
-                        <form action="Booking.php" method="post" class="form-body">
+                        <form action="../booking/Booking.php" method="post" class="form-body">
                             <div class="row form-row">
                               <input type="text" placeholder="Enter Full name" name="name" id="name" class="form-control">
                             </div>
@@ -69,6 +42,9 @@ a {
                             </div>
                              <div class="row form-row">
                               <input type="text" placeholder="Enter Email Adreess" name="email" id="email" class="form-control">
+                            </div>
+                           <div class="row form-row">
+                              <input id="dat" type="text" placeholder="Appointment Date" name="date" id="date" class="form-control">
                             </div>
                             <div class="row form-row">
                               <input type="time" placeholder="Enter Appointment Time" name="time" id="time" class="form-control">
@@ -112,14 +88,18 @@ a {
 
 </div>
 
-      
+
 </body>
 
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="../assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
-    <script src="../assets/js/popper.min.js" type="text/javascript"></script>
-    <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
 
+    <script>
+      $(document).ready(function(){
+          $("#dat").datepicker();
+      })
+    </script>
+    
 </html>
+   
