d61aab9facf6dcf62a7157578be79b99384abc9c
5009CEM-Quit-Smoking-App <nicholaswong613@gmail.com>
2022-11-18 11:48:27 +0800

    Second Commit By Nicholas Wong

--- services.php
+++ services.php
@@ -13,7 +13,7 @@ if ($mysqli->connect_error) {
 }
 
 // SQL query to select data from database
-$sql = " SELECT * FROM services ORDER BY id ASC ";
+$sql = " SELECT * FROM Services ORDER BY service_id ASC ";
 $result = $mysqli->query($sql);
 $mysqli->close();
 ?>
@@ -29,54 +29,42 @@ $mysqli->close();
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="style1.css" rel="stylesheet" type="text/css"/>
-        <link href="service.css" rel="stylesheet" type="text/css"/>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+
     </head>
 
   <body>
-    <?php include 'header.html';?>
+    <?php include 'includes/session.php';?>
+    <?php include 'navbar.php';?>
     
-  <br><br><p>We are more than just an ordinary salon. At Studio C Hair & Beauty Salon, you can get convenience, personalization and transformation at affordable prices. We provide you with all your hair needs under one roof. Whether you’re getting a quick trim, hair colored or a completely new look, we got you all covered here!</p>
-  <div class="main-box">
-                    <?php
-				while($rows=$result->fetch_assoc())
-				{
-			?>
+    <div class="container">
+        
+    <br/><h3>Our Services</h3>
     
-        <div class="box box-grey">
-            <img src="assets/img/<?php echo $rows['img']; ?>" alt="" height="100" width="150">
-            <h1><?php echo $rows['name'];?></h1>
-          <hr>
-          <p style="text-align:justify-all;"><?php echo $rows['description'];?></p>
-          <a href="index.php">Read More</a>
-        </div>
-          
-          <?php
-				}
-			?>
-            
-         <?php
-				while($rows=$result->fetch_assoc())
-				{
-			?>
+    <div class="box-container">
     
-        <div class="box box-red">
-            <img src="img/<?php echo $rows['img']; ?>" alt="" height="100" width="150">
-            <h2><?php echo $rows['name'];?></h2>
-          <hr>
-          <p style="text-align:justify-all;"><?php echo $rows['description'];?></p>
-          <a href="service.html"  class="white-border">Read More</a>
-        </div>
-          
-          <?php
-				}
-			?>
-                    
-      </div>
-      </div>
+    
+        <div class="box">
+    <?php
+		while($rows=$result->fetch_assoc())
+			{
+	?>
+            <img src="assets/img/services_images/<?php echo $rows['img']; ?>" alt="">
+            <h3><?php echo $rows['service_name'];?></h3>
+            <p>RM <?php echo $rows['service_price'];?></p>
+            <p><?php echo $rows['service_description'];?></p><hr>
+    <?php
+        }
+    ?>    </div>
 
-    <?php include 'footer.html';?>
+    </div>
 
-  </body>
+</div>
 
+    <?php include 'footer.html'?>
+
+    </body>
 </html>
-
