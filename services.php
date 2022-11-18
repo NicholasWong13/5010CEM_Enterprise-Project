a8dda3948edc36bfd71c76be767da3be4d4408a0
5009CEM-Quit-Smoking-App <nicholaswong613@gmail.com>
2022-11-18 15:34:10 +0800

    Updated By Nicholas Wong

--- services.php
+++ services.php
@@ -28,7 +28,7 @@ $mysqli->close();
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="style1.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="assets/css/style1.css"> 
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -64,7 +64,7 @@ $mysqli->close();
 
 </div>
 
-    <?php include 'footer.html'?>
+    <?php include 'footer.html';?>
 
     </body>
 </html>
